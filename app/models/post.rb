db03a3c2d1ef92f6c9524a260806ec5bfe7b550d
augusto <augusto@hardfunstudios.com>
2020-08-12 11:26:33 -0300

    Add vue

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,22 +1,10 @@
 class Post < ActiveRecord::Base
-    include WpCache
-    include WpPost
     
   def self.schedule_create_or_update(wp_id_from_params, params)
     puts "=================================="
     puts wp_id_from_params
     puts "-------"
-    puts params
+    puts params.to_json
   end
   
-    def update_wp_cache(json)
-        puts json
-        '''
-        update_post(json)
-        author = Author.find_or_create(json["author"]["ID"])
-        author.update_wp_cache(json["author"])
-        self.author = author
-        self.save
-        '''
-    end
 end
