0d9f366a3e0beba764211dab5a2652fa8f8a1adb
barbsmfe <barbara.ferreira@acad.pucrs.br>
2020-08-13 17:27:55 -0300

    Save post that comes from the hook

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,51 @@ class Post < ApplicationRecord
   def self.create_from_hook(params)
     puts "=================================="
     puts params.to_json
+    
+    json_post = JSON.parse(params.to_json)
+
+    post = Post.new
+
+    post.post_author = json_post.dig('post', 'post_author')
+    post.post_date_gmt = json_post.dig('post', 'post_date_gmt')
+    post.post_content = json_post.dig('post', 'post_content')
+
+    post.post_title = json_post.dig('post', 'post_title')
+    post.post_status = json_post.dig('post', 'post_status')
+    post.post_excerpt = json_post.dig('post', 'post_excerpt')
+
+    post.comment_status = json_post.dig('post', 'comment_status')
+    post.ping_status = json_post.dig('post', 'ping_status')
+    post.post_nama = json_post.dig('post', 'post_name')
+
+    post.post_modified = json_post.dig('post', 'post_modified')
+    post.post_modified_gmt = json_post.dig('post', 'post_modified_gmt')
+    post.post_content_filtered = json_post.dig('post', 'post_content_filtered')
+
+    post.post_parent = json_post.dig('post', 'post_parent')
+    post.post_wp_id = json_post.dig('post_id')
+    post.menu_order = json_post.dig('post', 'menu_order')
+
+    post.guid = json_post.dig('post', 'guid')
+    post.post_type = json_post.dig('post', 'post_type')
+    post.post_mime_type = json_post.dig('post', 'post_mime_type')
+
+    post.comment_count = json_post.dig('post', 'comment_count')
+    post.filter = json_post.dig('post', 'filter')
+    post.post_meta = json_post.dig('post_meta')
+
+    post.post_thumbnail = json_post.dig('post_thumbnail')
+
+    if post.save
+      post
+    else
+      raise ActiveRecord::RecordNotSaved.new "STOP Wordpress post creation" +
+        "Wordpress post can not be saved"
+    end
   end
   
   def self.update_from_hook(params)
+   
     
   end
 end
