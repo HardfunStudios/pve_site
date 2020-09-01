5577aa4d75daf9790fd380d5ed0fe68c23b290ab
augusto <augusto@hardfunstudios.com>
2020-09-01 17:31:59 -0300

    Fixing specs rubocop offenses

--- spec/requests/wpconnector_request_spec.rb
+++ spec/requests/wpconnector_request_spec.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Wpconnectors", type: :request do
+RSpec.describe 'Wpconnectors', type: :request do
   let(:filepath1) { Rails.root.join('spec/fixtures/post_sample1.json') }
   let(:filepath1_copy) { Rails.root.join('spec/fixtures/post_sample1_copy.json') }
-  
-  it 'creates a new post from webhook' do  
-    expect { 
+
+  it 'creates a new post from webhook' do
+    expect do
       post '/wp-post-webhook/post', params: JSON.parse(File.read(filepath1))
-    }.to change { Post.count }
+    end.to(change { Post.count })
   end
-  
+
   context 'when posting existing content' do
     before do
       post '/wp-post-webhook/post', params: JSON.parse(File.read(filepath1))
-    end    
-    
-    it 'does not create another post' do
-      expect { 
-        post '/wp-post-webhook/post', params: JSON.parse(File.read(filepath1_copy))
-      }.not_to change { Post.count }    
     end
-    
+
+    it 'does not create another post' do
+      expect do
+        post '/wp-post-webhook/post', params: JSON.parse(File.read(filepath1_copy))
+      end.not_to(change { Post.count })
+    end
+
     it 'updates the post from webhook' do
       id = 30 # id from post file on wordpress
       post_before = Post.where(wp_id: id).first
