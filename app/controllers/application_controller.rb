a5f5f9e81e2c11e3f75a6782db7fecbd2df877e8
augusto <augusto@hardfunstudios.com>
2020-09-10 09:04:50 -0300

    color header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :color_header
+  
+  private
+  
+  def color_header
+    @color = 'bg-color-purple'
+    if (params[:controller] == 'pages' && params[:action] == 'home')
+      @color = 'bg-transparent'
+    end
+  end
 end
