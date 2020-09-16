b2baa2d0d0e91187c43e9d333c596404725e50d9
barbsmfe <barbara.ferreira@acad.pucrs.br>
2020-09-16 16:53:37 -0300

    Menu done (hopefully)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,12 @@ class ApplicationController < ActionController::Base
   
   def color_header
     @color = 'bg-header-gestores'
+    @text_color = 'font-color-white'
+    @logo_header = 'logo-header-white'
     if (params[:controller] == 'pages' && params[:action] == 'home')
       @color = 'bg-header-home'
+      @text_color = 'font-color-purple'
+      @logo_header = 'logo-header-purple'
     end
   end
 end
